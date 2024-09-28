fb934d773967a5cc40b653358ac6bcb2a8e704fd
mansurserikov <Mansurserikov889@gmail.com>
2024-09-28 16:41:07 +0500

    test1

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Вывод строки на консоль
+        System.out.println("Hello, World!");
+
+        // Объявление переменных
+        int a = 10;
+        int b = 20;
+
+        // Выполнение базовых операций
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = b / a;
+
+        // Вывод результатов
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разница: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+    }
+}
