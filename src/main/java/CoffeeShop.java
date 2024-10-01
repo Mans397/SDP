1b1c697be201d6a2be2d33976a382e6996dac80e
mansurserikov <Mansurserikov889@gmail.com>
2024-10-01 22:56:54 +0500

    добавил комменты

--- src/main/java/CoffeeShop.java
+++ src/main/java/CoffeeShop.java
@@ -1,16 +1,17 @@
+//здесь юзаю паттерн одиночку
 public class CoffeeShop {
     private static CoffeeShop instance;
 
     private CoffeeShop() {}
 
-    public static CoffeeShop getInstance() {
+    public static CoffeeShop getInstance() { // для создания обьекта
         if (instance == null) {
             instance = new CoffeeShop();
         }
         return instance;
     }
 
-    public void takeOrder(Coffee coffee) {
+    public void takeOrder(Coffee coffee) { //метод для получения названия кофе
         System.out.println("Order received for: " + coffee.getName());
     }
 }
